39f1300a2a795ae7e140fe1ed45cfe4180098131
Dipu2309 <dileep.behera@yahoo.com>
2024-01-19 02:11:20 +0530

    Built in pipe

---
+++ Angular/8-built-in-pipe/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'product details';
+  productCode = 'PROD_P001';
+  productName = 'Laptop';
+}
